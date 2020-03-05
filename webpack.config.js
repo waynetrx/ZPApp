422caefd8be0a6c85e5389aad271ff6adab8e97b
Wayne Teo <waynetrx@pop-os.localdomain>
2020-03-05 23:27:54 +0800

    Import project from original source

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+            test: /\.(t|j)sx?$/,
+            use: {
+                loader: 'awesome-typescript-loader'
+            }
+        },
+        {
+            enforce: 'pre',
+            test: /\.js$/,
+            loader: 'source-map-loader'
+        }
+      ]
+    }
+  };
