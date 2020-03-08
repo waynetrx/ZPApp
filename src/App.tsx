78052ef0be9782394f19f45bea976367b7cf23bb
Wayne Teo <waynerulz90@gmail.com>
2020-03-09 00:47:01 +0800

    Pull data from api and load to barchart

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,50 @@
-import React from 'react';
-import logo from './zuelliglogo.png';
+import React, { useState, useEffect }  from 'react';
 import {BarChart} from './common/BarChart'
+import axios from "axios"
+
 import './App.css';
 
 function App() {
+  // const [state, setState] = useState([] as any)
+  const [state, setState] = useState({ chartData: []})
+  useEffect(() => {
+    console.log("App Component did mount executed")
+    let chartData = {
+      chartData: [{ "name": "SG", "value": 55}, { "name": "MY", "value": 22}]
+    }
+    //setState({...state, chartData})
+    let countries = ["United States", "Netherlands", "Singapore", "India", "China", "UK", "Colombia", "Germany", "Japan", "Italy"]
+
+    //let url = "https://cors-anywhere.herokuapp.com/http://universities.hipolabs.com/search?country=";
+    let url = "http://universities.hipolabs.com/search?country=";
+
+    let promises: Array<Promise<void>> = []
+    let universities = []
+    countries.forEach( value =>{
+      promises.push(
+        axios.get(url + value)
+        .then(res => {
+          universities = universities.concat(res.data)
+        })
+      )
+      
+    })
+
+    Promise.all(promises).then(() => {
+      console.log(universities.length)
+      setState(prevState => {
+        return { chartData: universities}
+      })
+    });
+    
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Zuellig Pharma Frontend Test
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
 
       <BarChart
-      
+        data={ state.chartData }
+        title="University Count By Country"
+        color="#70CAD1"
       />
     </div>
   );
--- src/common/BarChart.tsx
+++ src/common/BarChart.tsx
@@ -1,24 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import Chart from "chart.js";
+import React, { useState, useEffect, useRef } from 'react'
+import Chart from "chart.js"
 
 
 interface BarChartProps {
-    Data?: Array<number>,
-    ChartRef?: React.RefObject<HTMLCanvasElement>
+    data: Array<any>,
+    title: string,
+    color: string
+
 }
 
-export const BarChart: React.FC<BarChartProps> = ({ Data, ChartRef}) => {
-    const [state, setState] = useState([])
+const chartConfig = {
+    type: 'bar',
+    data: {
+        labels: [] as any,
+        datasets: [{
+            label: 'University Count By Country',
+            data: [] as any,
+            backgroundColor: '#112233'
+        }]
+    }
+}
 
-    useEffect(() => {
-        console.log("Component did mount executed")
-    })
-
-    const setCountryUniversityCount = data => {
-        setState(data)
+export const BarChart: React.FC<BarChartProps> = ({ data, title, color}) => {
+    const chartContainer: React.RefObject<HTMLCanvasElement> = useRef(null)
+    const [chartInstance, setChartInstance] = useState(null);
+    const parseChartData = data => {
+        console.log(data.length)
+        var result = data.reduce((total, n) => (
+            { 
+                ...total, 
+                [n.country]: total[n.country] ? total[n.country] + 1 : 1
+            }),{});
+        console.log(result)
+        return result
     }
 
+    useEffect(() => {
+        var result = parseChartData(data)
+        console.log(chartContainer)
+        if(chartContainer && chartContainer.current){
+            if(data && data.length > 0){
+                chartConfig.data.labels = Object.keys(result)
+                chartConfig.data.datasets[0].data = Object.values(result)
+            }
+            const newChartInstance = new Chart(chartContainer.current, chartConfig)
+            setChartInstance(newChartInstance)
+        }
+    }, [chartContainer, data])
+
+    
     return (
-    <canvas ref={ChartRef} />
+    <canvas ref={chartContainer} />
     )
 }
